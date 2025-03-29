3548cc596bfee142ba0e3462b45bbbb6e7cc4841
AmitPathak <amit7081483564@gmail.com>
2025-03-29 13:09:05 +0530

    Add multi-authentication with dashboard and registration functionality

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,28 +3,68 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
 
-    public function index()
+    public function loginPage()
     {
         return view('login');
     }
+    public function registerPage()
+    {
+        return view('register');
+    }
 
-    public function authenticate(Request $request)
+    public function processRegister(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        if ($validator->passes()) {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->role = 'user';
+            $user->save();
+
+            return redirect()->route('account.login')->with('success', 'You have Successfully Registed.');
+
+
+        } else {
+            return redirect()->route('account.register')->withErrors($validator)->withInput();
+        }
+    }
+
+    public function athenticate(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        if($validator->passes()){
-
-        }else{
+        if ($validator->passes()) {
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return redirect()->route('account.dashboard')->with('success', 'You have Successfully logged in.');
+            } else {
+                return redirect()->route('account.login')->with('error', 'Invalid email or password');
+            }
+        } else {
             return redirect()->route('account.login')->withErrors($validator)->withInput();
         }
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('account.login')->with('success', 'You have Successfully logged out.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,18 @@ Route::get('/', function () {
 });
 
 
-Route::get('login',[LoginController::class, 'index'])->name('account.login');
+Route::group(['prefix' => 'account'], function(){
+    
+    Route::group(['middleware' => 'guest'], function(){
+        Route::get('register',[LoginController::class, 'registerPage'])->name('account.register');
+        Route::get('login',[LoginController::class, 'loginPage'])->name('account.login');
+        Route::post('process-register',[LoginController::class, 'processRegister'])->name('account.processRegister');
+        Route::post('athenticate',[LoginController::class, 'athenticate'])->name('account.athenticate');
+    });
+
+    Route::group(['middleware' => 'auth'], function(){
+        Route::get('logout',[LoginController::class, 'logout'])->name('account.logout');
+        Route::get('dashboard',[DashboardController::class, 'index'])->name('account.dashboard');
+
+    });
+});
